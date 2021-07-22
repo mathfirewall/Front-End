34deb8f2aab9c5f8a562f53ab7a02478a22db73d
F3RR4LL <72284260+mathfirewall@users.noreply.github.com>
2021-07-22 14:26:29 -0300

    commit 2021

---
+++ produtos.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel = "shortcut icon" type="image/x-icon" href="imagens/favicon.ico">
+    <!---classe css editavel a ajustes -->
+    <link href="css/estilo.css" rel="stylesheet">
+    <!---classe css do bootstrap não mexer -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <title>Evolution Technology</title>
+</head>
+<body>
+
+  <header class="cabecalho">
+
+    <h1 class="titulo">Evolution Technology</h1>
+    
+   <!---esse container é usado pela nav os menus --> 
+   
+    <div class="container">
+      <nav class="navbar ">
+        <div style="padding-left: 25%;" >
+        <a class="btn btn-outline-dark" style="color: white;" href="index.php">Home</a>
+          <a class="btn btn-outline-dark" style="color: white;" href="sobre.php">Sobre</a>
+          <a class="btn btn-outline-dark" style="color: white;" href="produtos.php">Produtos</a>
+          <a class="btn btn-outline-dark" style="color: white;" href="servicos.php">Serviços</a>
+          <a class="btn btn-outline-dark" style="color: white;" href="faleconosco.php">Fale Conosco</a>
+      </div>
+      </nav>
+        </div>
+
+  </header>
+	
+	
+
+      <div>
+          <!---container usado pela card -->
+      <div class="container" style="padding-left: 6%;">
+        <!---card é a classs bootstrap -->
+        <div class="card cardp">
+         <img class="card-img-top cadimg" src="imagens/placamae.jpg" alt="Imagem de capa do card">
+         <div class="card-body" style="height: 21.2rem;">
+           <h5 class="card-title">Placa Mãe</h5>
+           <p class="card-text">Placa-Mãe Asus TUF B450M-Plus Gaming, AMD AM4, mATX, DDR4</p>
+           <a href="faleconosco.php" class="btn btn-primary" style="margin-top:90px;">Comprar</a>
+         </div>
+       </div>
+       <!---card é a classs bootstrap -->
+       <div class="card cardp">
+         <img class="card-img-top cadimg" src="imagens/processador.jpg" alt="Imagem de capa do card">
+         <div class="card-body" style="height: 18.3rem;">
+           <h5 class="card-title">Processador</h5>
+           <p class="card-text">Processador AMD Ryzen 5 5600X, Cache 35MB, 3.7GHz (4.6GHz Max Turbo), AM4 - 100-100000065BOX</p>
+           <a href="faleconosco.php" class="btn btn-primary btnnv " style="margin-top: -5px;">Comprar</a>
+         </div>
+       </div>
+       <!---card é a classs bootstrap -->
+       <div class="card cardp">
+         <img class="card-img-top cadimg" src="imagens/fonte.jpg" alt="Imagem de capa do card">
+         <div class="card-body" style="height: 18.3rem;">
+           <h5 class="card-title">Fonte</h5>
+           <p class="card-text">Fonte EVGA 600W 80 Plus White - 100-W1-0600-K</p>
+           <a href="faleconosco.php" class="btn btn-primary btnnv" style="margin-top:47px;">Comprar</a>
+         </div>
+       </div>
+
+       <!---card é a classs bootstrap -->
+       <div class="card cardp">
+         <img class="card-img-top cadimg" src="imagens/gabinete.jpg" alt="Imagem de capa do card">
+         <div class="card-body" style="height: 18.3rem;">
+           <h5 class="card-title">Gabinete Aerocool</h5>
+           <p class="card-text">Gabinete Aerocool Cylon RGB LED MID Tower ATX Preto</p>
+           <a href="faleconosco.php" class="btn btn-primary btnnv" style="margin-top:76px;" >Comprar</a>
+         </div>
+       </div>
+
+       <!---card é a classs bootstrap -->
+       <div class="card cardp">
+         <img class="card-img-top cadimg" src="imagens/ssd.jpg" alt="Imagem de capa do card">
+         <div class="card-body" style="height: 18.3rem;">
+           <h5 class="card-title">Armazenamento</h5>
+           <p class="card-text">SSD Crucial BX500, 480GB, SATA, Leitura 540MB/s, Gravação 500MB/s - CT480BX500SSD1</p>
+           <a href="faleconosco.php" class="btn btn-primary btnnv" style="margin-top:47px;">Comprar</a>
+         </div>
+       </div>
+
+       <!---card é a classs bootstrap -->
+       <div class="card cardp">
+         <img class="card-img-top cadimg" src="imagens/memoria.jpg" alt="Imagem de capa do card">
+         <div class="card-body" style="height: 18.3rem;">
+           <h5 class="card-title">Memoria</h5>
+           <p class="card-text">GMemória HyperX Fury RGB, 8GB, 3000MHz, DDR4, CL15, Preto - HX430C15FB3A/8</p>
+           <a href="faleconosco.php" class="btn btn-primary btnnv" style="margin-top:69px;">Comprar</a>
+         </div>
+       </div>
+   <!---card é a classs bootstrap -->
+       <div class="card cardp">
+         <img class="card-img-top cadimg" src="imagens/servidor1.jpg" alt="Imagem de capa do card">
+         <div class="card-body" style="height: 18.3rem;">
+           <h5 class="card-title">Servidor HP</h5>
+           <p class="card-text">Servidor HP Proliant ML350 Gen9 Xeon E5-2620 V3 2.4GHz Six Core 15MB Cache, Mem 8GB HD 300GB SAS SFF 10k, Fonte 500W PN: 776979-S05</p>
+           <a href="faleconosco.php" class="btn btn-primary btnnv">Comprar</a>
+         </div>
+       </div>
+   <!---card é a classs bootstrap -->
+       <div class="card cardp">
+         <img class="card-img-top cadimg" src="imagens/servidor2.jpg" alt="Imagem de capa do card">
+         <div class="card-body" style="height: 18.4rem;">
+           <h5 class="card-title">Servidor Lenovo</h5>
+           <p class="card-text">Servidor Lenovo DCG TS150 E3-1225v6 3.3 Ghz 8GB 1000GB 70UB002CBR</p>
+           <a href="faleconosco.php" class="btn btn-primary btnnv" style="margin-top:75px;">Comprar</a>
+         </div>
+       </div>
+   <!---card é a classs bootstrap -->
+       <div class="card cardp">
+         <img class="card-img-top cadimg" src="imagens/servidor3.jpg" alt="Imagem de capa do card">
+         <div class="card-body">
+           <h5 class="card-title">Servidor Dell</h5>
+           <p class="card-text">Servidor PowerEdge T340, BCC</p>
+           <a href="faleconosco.php" class="btn btn-primary btnnv" style="margin-top:10px;">Comprar</a>
+         </div>
+       </div>
+
+       
+     </div>
+
+    </div>
+       
+      
+      <footer>
+
+          <p>Criado Por Thiago Araújo -> Aluno Da Estácio - AM</p>
+      </footer> 
+      
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+      <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
