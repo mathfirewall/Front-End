5cacebb007b070a62bd809ca732d00db7a8259d3
F3RR4LL <72284260+mathfirewall@users.noreply.github.com>
2023-01-05 12:21:20 -0400

    Update conexao.php
    atualizando alguns recursos

--- conexao.php
+++ conexao.php
@@ -1,8 +1,8 @@
 <?php
 
-define('HOST', '127.0.0.1');
-define('USER', 'root');
-define('PASS', 'Angelamoss20182018');
+define('HOST', '');
+define('USER', '');
+define('PASS', '');
 define('DB', 'evolution');
 
 $conexao = mysqli_connect(HOST, USER, PASS, DB) or die('Opss....');
