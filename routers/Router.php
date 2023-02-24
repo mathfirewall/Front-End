afa6f48f4377a08529f806b830e3c20dd4a70aaa
mathfirewall <thiagodevmobile@gmail.com>
2023-02-24 16:20:32 -0300

    update routers controllers

--- routers/Router.php
+++ routers/Router.php
@@ -1,7 +1,7 @@
 <?php
 
-//require __DIR__ . '../controllers/LoginController';
-class Router extends RouteSwitch
+require __DIR__ . '/../controllers/LoginController';
+class Router extends LoginController
 {
     public function run(string $requestUri)
     {
@@ -16,45 +16,6 @@ class Router extends RouteSwitch
 }
 
 
-abstract class RouteSwitch
-{
-    protected function login()
-    {
-       require __DIR__ . '/../views/Login.php';
-        //echo "Teste";
 
-    }
-
-    protected function cadUser()
-    {
-        require __DIR__ . '/insereUsers.php';
-    }
-
-    protected function teste()
-    {
-        require __DIR__ . '/getUsers.php';
-    }
-
-    protected function produtosAll()
-    {
-        require __DIR__ . '/consulta.php';
-    }
-
-    protected function produtos($id)
-    {
-        //require __DIR__ . '/consulta.php';
-        
-        echo $id;
-    }
-
-    
-    function __call($name, $arguments)
-    {
-        http_response_code(404);
-       // $resposta[] = "Nada Encontrado";
-        //echo json_encode($resposta);
-        return $this->home();
-    }
-}
 
 
